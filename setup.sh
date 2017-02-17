d0889f395d1720ab0053e4bdbbeac51cc42ddc14
Satoshi Ogata <satosystems@gmail.com>
2017-02-17 19:29:12 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 if [ "`pwd`" != "$(cd $(dirname $0) && pwd)" ]; then
   tput setaf 1 && echo "Usage:"
@@ -7,17 +7,37 @@ if [ "`pwd`" != "$(cd $(dirname $0) && pwd)" ]; then
   exit 1
 fi
 
-dotfiles=`ls -dFG .* | grep -v -e "./" -e "../" -e ".*.swp" -e ".git/"`
+# create symblic files
+dotfiles=`ls -dFG .* | grep -v -e "./" -e ".*.swp"`
 
 for dotfile in $dotfiles; do
-  echo $dotfile
+  echo "$dotfile => $HOME/$dotfile"
   rm -rf "$HOME/$dotfile"
-  ln -s `pwd`/$dotfile "$HOME/$dotfile"
+  ln -s "`pwd`/$dotfile" "$HOME/$dotfile"
+done
+
+# create nvim of xdg-config-home
+[ -d "$HOME/.config/nvim" ] || mkdir -p "$HOME/.config/nvim"
+
+# create symbolic directories
+declare -A dirs
+
+dirs[".config/nvim"]="$HOME/.config/nvim"
+
+for i in ${!dirs[@]}; do
+  echo "${i} => ${dirs[$i]}"
+  rm -rf "${dirs[$i]}"
+  ln -s "`pwd`/${i}" "${dirs[$i]}"
 done
 
 # download git-completion.bash and git-prompt.sh from GitHub master branch
-rm -f ~/.git-completion.bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
-rm -f ~/.git-prompt.sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
+declare -A urls
+
+urls[".git-completion.bash"]="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+urls[".git-prompt.sh"]="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+
+for i in ${!urls[@]}; do
+  echo "${urls[$i]} => ${i}"
+  curl ${urls[$i]} > "$HOME/${i}"
+done
 
